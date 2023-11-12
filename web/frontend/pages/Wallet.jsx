8cce38c943ee1521048dfba2d553ff640d50f74d
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-12 10:53:19 +0800

    feat: tried loading page

--- web/frontend/pages/Wallet.jsx
+++ web/frontend/pages/Wallet.jsx
@@ -14,6 +14,7 @@ import {
   Frame,
   Modal,
   Link,
+  Loading,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
 import { useTranslation } from "react-i18next";
@@ -77,168 +78,9 @@ export default function Wallet() {
   };
 
   return loading ? (
-    <SkeletonPage fullWidth>
-      <Layout>
-        {/* first half */}
-        <Layout.Section>
-          {/* explanation card */}
-          <AlphaCard sectioned>
-            <div
-              style={{
-                width: "100%",
-                height: "30vh",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <div
-                style={{
-                  width: "80%",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <SkeletonDisplayText size="medium" />
-                <div style={{ padding: "48px" }}>
-                  <SkeletonThumbnail size="large" />
-                </div>
-                <SkeletonBodyText />
-              </div>
-            </div>
-          </AlphaCard>
-        </Layout.Section>
-
-        {/* number of tokens card */}
-        <Layout.Section oneThird>
-          <AlphaCard sectioned>
-            <div
-              style={{
-                width: "100%",
-                height: "30vh",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Text variant="bodyLg">You currently have:</Text>
-              <Text variant="heading3xl">
-                <p
-                  style={{
-                    padding: "24px",
-                    fontSize: "48px",
-                    fontWeight: "bold",
-                  }}
-                >
-                  0
-                </p>
-              </Text>
-              <Text>Tokens</Text>
-
-              {/* {tokenQuantity < 10 ? ( */}
-              <div style={{ marginTop: "16px" }}>
-                <Text tone="subdued" variant="bodySm">
-                  Maybe it's time to stock up.
-                </Text>
-              </div>
-              {/* ) : null} */}
-            </div>
-          </AlphaCard>
-        </Layout.Section>
-      </Layout>
-
-      {/* second half */}
-      <div style={{ marginTop: "16px" }}>
-        <Layout>
-          <Layout.Section>
-            {/* form card */}
-            <AlphaCard sectioned>
-              <div
-                style={{
-                  width: "100%",
-                  height: "50vh",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <div
-                  style={{
-                    width: "80%",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <Text variant="headingLg" as="h1">
-                    {t("Wallet.formHeading")}
-                  </Text>
-                  <div style={{ width: "100%", marginTop: "16px" }}>
-                    <Form>
-                      <FormLayout>
-                        <TextField
-                          label="Number of tokens"
-                          suffix="tokens"
-                          type="number"
-                          value={tokenQuantity}
-                          onChange={handleTokenQuantityChange}
-                          autoComplete="off"
-                          min={1}
-                          error={error}
-                          onBlur={validate}
-                        />
-                        <div style={{ paddingTop: "16px", width: "100%" }}>
-                          <Button
-                            primary
-                            destructive
-                            fullWidth
-                            onClick={openModal}
-                            ref={activator}
-                            disabled={disabled}
-                          >
-                            Buy tokens
-                          </Button>
-                        </div>
-                      </FormLayout>
-                    </Form>
-                  </div>
-                </div>
-              </div>
-            </AlphaCard>
-          </Layout.Section>
-
-          {/* wallet card */}
-          <Layout.Section oneThird>
-            <AlphaCard sectioned>
-              <div
-                style={{
-                  width: "100%",
-                  height: "50vh",
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Text variant="bodyLg">You do not have a wallet.</Text>
-                <div style={{ paddingTop: "16px", paddingBottom: "8px" }}>
-                  <Button primary url="/connect-wallet">
-                    Connect a wallet
-                  </Button>
-                </div>
-                <Button url="/create-wallet">Create a wallet</Button>
-              </div>
-            </AlphaCard>
-          </Layout.Section>
-        </Layout>
-      </div>
-    </SkeletonPage>
+    <Frame>
+      <Loading />
+    </Frame>
   ) : (
     <Page fullWidth>
       <Frame>
