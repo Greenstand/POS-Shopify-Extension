2c925bca51c2663d714480d945b294dc4f863137
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-11-12 21:22:46 +0800

    feat: added api to create wallet

--- web/routes/wallet/create-wallet.js
+++ web/routes/wallet/create-wallet.js
@@ -1,7 +1,10 @@
 import apiClient from "../../utils/apiClient.js";
+import { updateMetafield } from "../../utils/metafield.js";
 
 export const createWallet = async (req, res) => {
+  const session = res.locals.shopify.session;
   const auth = apiClient.isAuthenticated();
+  const { walletName, shopName, shopOwnerName, shopEmail } = req.query;
 
   if (!auth) {
     try {
@@ -18,5 +21,50 @@ export const createWallet = async (req, res) => {
         error: err,
       });
     }
+  }
+
+  try {
+    const wallet = await apiClient.post("/wallets", {
+      wallet: walletName,
+    });
+
+    const meta1 = await updateMetafield(
+      session,
+      "$app:wallet",
+      "walletId",
+      wallet.id,
+      "string",
+    );
+    const meta2 = await updateMetafield(
+      session,
+      "$app:wallet",
+      "shopName",
+      shopName,
+      "string",
+    );
+    const meta3 = await updateMetafield(
+      session,
+      "$app:wallet",
+      "shopOwnerName",
+      shopOwnerName,
+      "string",
+    );
+    const meta4 = await updateMetafield(
+      session,
+      "$app:wallet",
+      "shopEmail",
+      shopEmail,
+      "string",
+    );
+
+    console.log(meta1, meta2, meta3, meta4);
+
+    return res.status(200).send({ data: wallet });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).send({
+      error: err,
+    });
   }
 };
