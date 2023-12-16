2c1828286c7a53573f63948bba3899b311f7fe81
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-16 10:49:09 +0800

    feat: added api endpoint called save-details.js

---
+++ web/routes/checkout/save-details.js
@@ -0,0 +1,19 @@
+import { updateMetafield } from "../../utils/metafield.js";
+
+export const save_details = async (req, res) => {
+  try {
+    const type = "single_line_text_field";
+    const namespace = "checkout";
+    const key = "offer";
+    const { value } = req.body;
+    const session = res.locals.shopify.session;
+
+    const meta = updateMetafield(session, namespace, key, value, type);
+
+    res
+      .status(200)
+      .send({ message: "Successfully updated value", error: false });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+};
