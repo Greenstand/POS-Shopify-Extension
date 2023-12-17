24de327e4e2fcf39eac1392365e2fe254fd793cd
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-17 14:58:59 +0800

    chore: removed unused variables from index.jsx

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,19 +1,11 @@
 import {
-  Card,
   Page,
   Layout,
-  TextContainer,
-  Image,
-  Stack,
   Link,
   Text,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
-import { useTranslation, Trans } from "react-i18next";
-
-import { trophyImage } from "../assets";
-
-import { ProductsCard } from "../components";
+import { useTranslation } from "react-i18next";
 
 export default function HomePage() {
   const { t } = useTranslation();
