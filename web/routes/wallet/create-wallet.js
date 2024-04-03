ba0bba16efcbfd6316d200cb96af2d3df8d0ba4f
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2024-04-03 11:06:40 +0800

    chore: clean create-wallet.js

--- web/routes/wallet/create-wallet.js
+++ web/routes/wallet/create-wallet.js
@@ -1,6 +1,11 @@
 import apiClient from "../../utils/apiClient.js";
 import { updateMetafield } from "../../utils/metafield.js";
 
+// ! func: createWallet (3 params)
+// ? req, res
+// * CRUD type: post
+// * return value: null
+
 export const createWallet = async (req, res) => {
   const session = res.locals.shopify.session;
   const auth = apiClient.isAuthenticated();
