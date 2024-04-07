4e71979864b703556aeb2ec340bb150df569536f
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2024-04-07 08:55:57 +0800

    feat: add helper auth function

--- web/tests/apiClient.test.js
+++ web/tests/apiClient.test.js
@@ -1,2 +1,27 @@
 import apiClient from "../utils/apiClient.js";
 import "dotenv/config";
+import { describe, it, expect } from "jest";
+
+const authenticate = () => {
+  if (apiClient.defaults.headers.common["Authorization"]) {
+    return 202;
+  }
+
+  apiClient
+    .post("/auth", {
+      wallet: `${process.env.TREETRACKER_WALLET_NAME}`,
+      password: `${process.env.TREETRACKER_WALLET_PASSWORD}`,
+    })
+    .then((data) => {
+      const { token } = data.data;
+
+      apiClient.setAuthToken(token);
+
+      return 201;
+    })
+    .catch((err) => {
+      console.log(err);
+
+      return err;
+    });
+};
--- web/tests/extensions.test.js
+++ web/tests/extensions.test.js
@@ -1,2 +1,3 @@
 import apiClient from "../utils/apiClient.js";
 import "dotenv/config";
+import { describe, it, expect } from "jest";
